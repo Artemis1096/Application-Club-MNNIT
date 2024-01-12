6e71200e9268712f0d7a21a86302b9cfa10df187
Jashanpreet Singh <jashanpreetsingh1096@gmail.com>
2024-01-12 12:26:41 +0530

    day 10 comments added

--- POTD/Day 10/quickSort.cpp
+++ POTD/Day 10/quickSort.cpp
@@ -1,39 +1,38 @@
-/* basically the idea is to select an element(pivot) and put it into its right place
-    then make its right and left partitions in a way such that the left side contains
-    elements less than the pivot and right side greater than the pivot
-*/
-class QuickSort{
-    void quickSort(int arr[], int low, int high)
-    {
-        if(low>=high){
-            return;
-        }
-        int p=partition(arr,low,high);
-        
-        quickSort(arr,low,p-1); // left partition
+/*
+You are given a permutation† 𝑝 of length 𝑛 and a positive integer 𝑘≤𝑛.In one operation, you:
+Choose 𝑘 distinct elements 𝑝𝑖1,𝑝𝑖2,…,𝑝𝑖𝑘.
+Remove them and then add them sorted in increasing order to the end of the permutation. 
+For example, if 𝑝=[2,5,1,3,4]
+and 𝑘=2 and you choose 5 and 3 as the elements for the operation, then [2,5,1,3,4]→[2,1,4,3,5].
+Find the minimum number of operations needed to sort the permutation in increasing order. 
+It can be proven that it is always possible to do so.
 
-        quickSort(arr,p+1,high); // right partition
+Time Complexity : O(n)
+Space Complexity : O(n)
+*/
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc;
+    cin>>tc;
+    while(tc--){
+        int n,k;
+        cin>>n>>k;
+        vector<int> x;
+        for(int i=0;i<n;i++){
+            int num;
+            cin>>num;
+            x.push_back(num);
+        }
+        int count=1;
+        for(int i=0;i<n;i++){
+            if(x[i]==count){count++;}
+        }
+        cout<<((n-count+k)/k)<<endl;
     }
-    
-    public:
-    int partition (int arr[], int low, int high)
-    {
-       int pivot=arr[low];
-       int c=0;
-       for(int i= low+1;i<=high;i++){
-           if(pivot>=arr[i]){
-               c++;
-           }
-       }
-       swap(arr[low],arr[low+c]);
-       int i=low,j=high;
-       int pidx=low+c;
-       while(i<pidx && j>pidx){
-           while(arr[i]>=pivot){i++;}
-           while(arr[j]<pivot){j--;}
-           if(i<pidx && j>pidx){
-               swap(arr[i],arr[j]);
-               i++;j--;
-           }
-       }
-    }
+    return 0;
+}
